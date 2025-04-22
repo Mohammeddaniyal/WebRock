656598320e3569f722aa7d0709b52d1ef87fe036
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-22 15:33:54 +0530

    Updated TMWebRock for handling different type of request

--- WEB-INF/classes/com/thinking/machines/webrock/TMWebRock.java
+++ WEB-INF/classes/com/thinking/machines/webrock/TMWebRock.java
@@ -3,25 +3,64 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.lang.reflect.*;
 import com.thinking.machines.webrock.pojo.Service;
+import com.thinking.machines.webrock.annotations.POST;
+import com.thinking.machines.webrock.annotations.GET;
 import com.thinking.machines.webrock.model.WebRockModel;
 public class TMWebRock extends HttpServlet
 {
     public void doGet(HttpServletRequest request,HttpServletResponse response)
     {
         try{
+            System.out.println("GET TYPE REQUEST");
             WebRockModel webRockModel=(WebRockModel)getServletContext().getAttribute("webRockModel");
-            //webRockModel.print();
+            String path=request.getPathInfo();
+            Service service=webRockModel.getService(path);
+            Class serviceClass=service.getServiceClass();
+            if(serviceClass.isAnnotationPresent(POST.class))
+            {
+                response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+            }
+            Method serviceMethod=service.getService();
+            if(serviceClass.isAnnotationPresent(POST.class))
+            {
+                response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+            }
             System.out.println(request.getPathInfo());
             int a=Integer.parseInt(request.getParameter("a"));
             int b=Integer.parseInt(request.getParameter("b"));
             System.out.println("Values : "+a+","+b);
-            String path=request.getPathInfo();
-            Service service=webRockModel.getService(path);
-            Class serviceClass=service.getServiceClass();
-            Method serviceMethod=service.getService();
+            
             Object obj=serviceClass.newInstance();
             Object result=serviceMethod.invoke(obj,a,b);
             System.out.println("Result : "+result);
+        }catch(Exception e){System.out.println(e);}
+    }
+    public void doPost(HttpServletRequest request,HttpServletResponse response)
+    {
+        try{
+            System.out.println("POST TYPE REQUEST");
+            WebRockModel webRockModel=(WebRockModel)getServletContext().getAttribute("webRockModel");
+            String path=request.getPathInfo();
+            Service service=webRockModel.getService(path);
+            Class serviceClass=service.getServiceClass();
+            if(serviceClass.isAnnotationPresent(GET.class))
+            {
+                response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+            }
+            Method serviceMethod=service.getService();
+            if(serviceClass.isAnnotationPresent(GET.class))
+            {
+                response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+            }
+            System.out.println(request.getPathInfo());
+            int a=Integer.parseInt(request.getParameter("a"));
+            int b=Integer.parseInt(request.getParameter("b"));
+            System.out.println("Values : "+a+","+b);
+            
+            Object obj=serviceClass.newInstance();
+            Object result=serviceMethod.invoke(obj,a,b);
+            System.out.println("Result : "+result);
+ 
         }catch(Exception e){System.out.println(e);}
     }
 }
