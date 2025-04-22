583fac6afcf2dd21707d3dab38ebc5c177453d79
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-22 11:43:33 +0530

    written the Service and got the Respected Service object accordingly the Path

--- WEB-INF/classes/com/thinking/machines/webrock/TMWebRock.java
+++ WEB-INF/classes/com/thinking/machines/webrock/TMWebRock.java
@@ -1,6 +1,7 @@
 package com.thinking.machines.webrock;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.lang.reflect.*;
 import com.thinking.machines.webrock.pojo.Service;
 import com.thinking.machines.webrock.model.WebRockModel;
 public class TMWebRock extends HttpServlet
@@ -11,8 +12,16 @@ public class TMWebRock extends HttpServlet
             WebRockModel webRockModel=(WebRockModel)getServletContext().getAttribute("webRockModel");
             //webRockModel.print();
             System.out.println(request.getPathInfo());
+            int a=Integer.parseInt(request.getParameter("a"));
+            int b=Integer.parseInt(request.getParameter("b"));
+            System.out.println("Values : "+a+","+b);
             String path=request.getPathInfo();
             Service service=webRockModel.get(path);
+            Class serviceClass=service.getServiceClass();
+            Method serviceMethod=service.getService();
+            Object obj=serviceClass.newInstance();
+            Object result=serviceMethod.invoke(obj,a,b);
+            System.out.println("Result : "+result)
         }catch(Exception e){System.out.println(e);}
     }
 }
