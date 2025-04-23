1b86af4eee102daf649c00d499912a8a03642774
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-23 12:15:18 +0530

    fixed import package

--- WEB-INF/classes/com/thinking/machines/webrock/annotations/Forward.java
+++ WEB-INF/classes/com/thinking/machines/webrock/annotations/Forward.java
@@ -1,5 +1,5 @@
 package com.thinking.machines.webrock.annotations;
-import java.lang.annotations.*;
+import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Forward
