4d5965ce26c3635877d0a74c81f6ee16236aa654
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-24 15:39:58 +0530

    Formatted the selection using vscode

--- WEB-INF/classes/com/thinking/machines/webrock/TMWebRockStarter.java
+++ WEB-INF/classes/com/thinking/machines/webrock/TMWebRockStarter.java
@@ -1,4 +1,5 @@
 package com.thinking.machines.webrock;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.lang.reflect.*;
@@ -7,9 +8,9 @@ import com.thinking.machines.webrock.pojo.*;
 import com.thinking.machines.webrock.model.*;
 import com.thinking.machines.webrock.annotations.*;
 import java.util.logging.*;
-public class TMWebRockStarter extends HttpServlet
-{
-private static final Logger logger = Logger.getLogger(TMWebRockStarter.class.getName());
+
+public class TMWebRockStarter extends HttpServlet {
+    private static final Logger logger = Logger.getLogger(TMWebRockStarter.class.getName());
 
     static {
         try {
@@ -22,128 +23,125 @@ private static final Logger logger = Logger.getLogger(TMWebRockStarter.class.get
         }
     }
 
+    public void init() throws ServletException {
+        super.init();
+        System.out.println("hi");
+        WebRockModel webRockModel = new WebRockModel();
+        getServletContext().setAttribute("webRockModel", webRockModel);
+        getServletContext().setAttribute("hello", "How are you");
 
-public void init() throws ServletException
-{
-super.init();
-System.out.println("hi");
-WebRockModel webRockModel=new WebRockModel();
-getServletContext().setAttribute("webRockModel",webRockModel);
-getServletContext().setAttribute("hello","How are you");
+        String rootFolderName = getServletContext().getInitParameter("SERVICE_PACKAGE_PREFIX");
+        if (rootFolderName == null) {
+            logger.severe("No root folder specified");
+            throw new ServletException("No root folder specified");
+        }
+        String absolutePath = getServletContext().getRealPath("/WEB-INF/classes/" + rootFolderName);
+        if (absolutePath == null) {
+            logger.severe("Root folder not found in web app");
+            throw new ServletException("Root folder not found in web app");
+        }
 
-String rootFolderName=getServletContext().getInitParameter("SERVICE_PACKAGE_PREFIX");
-if(rootFolderName==null)
-{
-logger.severe("No root folder specified");
-throw new ServletException("No root folder specified");
-}
-String absolutePath=getServletContext().getRealPath("/WEB-INF/classes/"+rootFolderName);
-if(absolutePath==null)
-{
-logger.severe("Root folder not found in web app");
-throw new ServletException("Root folder not found in web app");
-}
+        File rootFolder = new File(absolutePath);
+        if (!rootFolder.exists() || !rootFolder.isDirectory()) {
+            logger.severe("Specified root is not a valid directory: " + absolutePath);
+            throw new ServletException("Specified root is not a valid directory: " + absolutePath);
+        }
+        loadServices(rootFolder);
+    }
 
-File rootFolder=new File(absolutePath);
-if(!rootFolder.exists() || !rootFolder.isDirectory())
-{
-           logger.severe("Specified root is not a valid directory: " + absolutePath);
-throw new ServletException("Specified root is not a valid directory: "+absolutePath);
-}
-loadServices(rootFolder);
-}
-private void loadServices(File folder) throws ServletException
-{
-if(folder==null || !folder.exists()) return;
+    private void loadServices(File folder) throws ServletException {
+        if (folder == null || !folder.exists())
+            return;
 
-File files[]=folder.listFiles();
-for(File file:files)
-{
-if(file.isDirectory())
-{
-loadServices(file);
-}
-else
-{
-if(!file.getName().endsWith(".class")) continue;
-System.out.println("Found file: " + file.getAbsolutePath());
-Class serviceClass=getServiceClass(file);
-if(serviceClass==null) continue;
-WebRockModel webRockModel=(WebRockModel)getServletContext().getAttribute("webRockModel");
+        File files[] = folder.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                loadServices(file);
+            } else {
+                if (!file.getName().endsWith(".class"))
+                    continue;
+                System.out.println("Found file: " + file.getAbsolutePath());
+                Class serviceClass = getServiceClass(file);
+                if (serviceClass == null)
+                    continue;
+                WebRockModel webRockModel = (WebRockModel) getServletContext().getAttribute("webRockModel");
 
-Path path=(Path)serviceClass.getAnnotation(Path.class);
-if(path==null) continue;
+                Path path = (Path) serviceClass.getAnnotation(Path.class);
+                if (path == null)
+                    continue;
 
-boolean isGetAllowedOnClass=serviceClass.isAnnotationPresent(GET.class);
+                boolean isGetAllowedOnClass = serviceClass.isAnnotationPresent(GET.class);
 
-boolean isPostAllowedOnClass=serviceClass.isAnnotationPresent(POST.class);
+                boolean isPostAllowedOnClass = serviceClass.isAnnotationPresent(POST.class);
 
+                Method[] methods = serviceClass.getMethods();
+                for (Method method : methods) {
+                    Path p = method.getAnnotation(Path.class);
+                    OnStartUp onStartUp = method.getAnnotation(OnStartUp.class);
+                    if (p == null && onStartUp == null)
+                        continue;
+                    boolean runOnStart = (onStartUp != null);
+                    boolean isGetAllowed = isGetAllowedOnClass;
+                    boolean isPostAllowed = isPostAllowedOnClass;
+                    // giving priority to method level annotation GET/POST
+                    if (method.isAnnotationPresent(GET.class) || method.isAnnotationPresent(POST.class)) {
+                        System.out.println("Either GET/POST Present : " + p.value());
+                        isGetAllowed = method.isAnnotationPresent(GET.class);
+                        isPostAllowed = method.isAnnotationPresent(POST.class);
+                    } else if (isGetAllowedOnClass == false && isPostAllowedOnClass == false)
+                    // in case of no annotaiton
+                    // present either on class
+                    // or method allow both
+                    {
+                        System.out.println(path.value());
+                        isGetAllowed = isPostAllowed = true;
+                    }
 
-Method []methods=serviceClass.getMethods();
-for(Method method:methods)
-{
-Path p=method.getAnnotation(Path.class);
-OnStartUp onStartUp=method.getAnnotation(OnStartUp.class);
-if(p==null && onStartUp==null) continue;
+                    Forward forward = null;
+                    if (runOnStart == false) {
+                        forward = method.getAnnotation(Forward.class);
+                    }
+                    Service service = new Service();
+                    service.setServiceClass(serviceClass);
+                    service.setService(method);
+                    service.setPath(path.value() + p.value());
+                    service.setIsGetAllowed(isGetAllowed);
+                    service.setIsPostAllowed(isPostAllowed);
+                    
+                    if (forward != null)
+                        service.setForwardTo(forward.value());
+                    logger.info("Path : " + path.value() + p.value());
+                    System.out.println("---------------");
+                    System.out.println("Path : " + path.value() + p.value());
+                    System.out.println(isGetAllowed + "," + isPostAllowed);
+                    System.out.println("---------------");
+                    webRockModel.putService(service);
+                }
+            }
+        }
 
-boolean isGetAllowed=isGetAllowedOnClass;
-boolean isPostAllowed=isPostAllowedOnClass;
-//giving priority to method level annotation GET/POST
-if(method.isAnnotationPresent(GET.class) || method.isAnnotationPresent(POST.class))
-{
-    System.out.println("Either GET/POST Present : "+p.value());
-    isGetAllowed=method.isAnnotationPresent(GET.class);
-    isPostAllowed=method.isAnnotationPresent(POST.class);
-}else if(isGetAllowedOnClass==false && isPostAllowedOnClass==false)//in case of no annotaiton present either on class or method allow both
-{
-    System.out.println(path.value());
-    isGetAllowed=isPostAllowed=true;
-}
+    }
 
-Forward forward=method.getAnnotation(Forward.class);
-
-Service service=new Service();
-service.setServiceClass(serviceClass);
-service.setService(method);
-service.setPath(path.value()+p.value());
-service.setIsGetAllowed(isGetAllowed);
-service.setIsPostAllowed(isPostAllowed);
-if(forward!=null) service.setForwardTo(forward.value());
-logger.info("Path : " + path.value() + p.value());
-System.out.println("---------------");
-System.out.println("Path : "+path.value()+p.value());
-System.out.println(isGetAllowed+","+isPostAllowed);
-System.out.println("---------------");
-webRockModel.putService(service);
-}
-}
-}
-
-}
-private Class getServiceClass(File file) throws ServletException
-{
-try
-{
-String absolutePath=file.getAbsolutePath();
-String rootFolderName=getServletContext().getInitParameter("SERVICE_PACKAGE_PREFIX");
-int index=absolutePath.indexOf(rootFolderName+"\\");
-if(index==-1)
-{
-logger.severe("Root folder not found in path : " + rootFolderName + " , " + absolutePath);
-throw new ServletException("Root folder not found in path : "+rootFolderName+" , "+absolutePath);
-}
-//c:\tomcat9\webapps\dan\WEB-INF\classes\booby\com...
-// +1 for backslash
-//index+=rootFolderName.length()+1;
-String packageName=absolutePath.substring(index).replace("\\",".");
-packageName=packageName.substring(0,packageName.lastIndexOf("."));
- logger.info("packageName : " + packageName);
-System.out.println("packageName : "+packageName);
-return Class.forName(packageName);
-}catch(Exception e)
-{
+    private Class getServiceClass(File file) throws ServletException {
+        try {
+            String absolutePath = file.getAbsolutePath();
+            String rootFolderName = getServletContext().getInitParameter("SERVICE_PACKAGE_PREFIX");
+            int index = absolutePath.indexOf(rootFolderName + "\\");
+            if (index == -1) {
+                logger.severe("Root folder not found in path : " + rootFolderName + " , " + absolutePath);
+                throw new ServletException("Root folder not found in path : " + rootFolderName + " , " + absolutePath);
+            }
+            // c:\tomcat9\webapps\dan\WEB-INF\classes\booby\com...
+            // +1 for backslash
+            // index+=rootFolderName.length()+1;
+            String packageName = absolutePath.substring(index).replace("\\", ".");
+            packageName = packageName.substring(0, packageName.lastIndexOf("."));
+            logger.info("packageName : " + packageName);
+            System.out.println("packageName : " + packageName);
+            return Class.forName(packageName);
+        } catch (Exception e) {
             logger.severe("Error loading class: " + e.getMessage());
-return null;
-}
-}
+            return null;
+        }
+    }
 }
