9a9fd9b85dad28e4adb9a67d23289179b1a4bfad
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-24 15:40:10 +0530

    Formatted the selection using vscode

--- WEB-INF/classes/com/thinking/machines/webrock/pojo/Service.java
+++ WEB-INF/classes/com/thinking/machines/webrock/pojo/Service.java
@@ -1,84 +1,89 @@
 package com.thinking.machines.webrock.pojo;
+
 import java.lang.reflect.*;
-public class Service
-{
-private Class serviceClass;
-private  String path;
-private Method service;
-private boolean isGetAllowed;
-private boolean isPostAllowed;
-private String forwardTo;
-private boolean runOnStart;
-private int priority;
-public Service()
-{
-this.serviceClass=null;
-this.path="";
-this.service=null;
-this.forwardTo=null;
-}
-public void setServiceClass(java.lang.Class serviceClass)
-{
-this.serviceClass=serviceClass;
-}
-public java.lang.Class getServiceClass()
-{
-return this.serviceClass;
-}
-public void setPath(java.lang.String path)
-{
-this.path=path;
-}
-public void setIsGetAllowed(boolean isGetAllowed)
-{
-    this.isGetAllowed=isGetAllowed;
-}
-public void setIsPostAllowed(boolean isPostAllowed)
-{
-    this.isPostAllowed=isPostAllowed;
-}
-public void setForwardTo(String forwardTo)
-{
-    this.forwardTo=forwardTo;
-}
-public void setRunOnStart(boolean runOnStart)
-{
-    this.runOnStart=runOnStart;
-}   
-public void setPriority(int priority)
-{
-    this.priority=priority;
-}
-public java.lang.String getPath()
-{
-return this.path;
-}
-public void setService(java.lang.reflect.Method service)
-{
-this.service=service;
-}
-public java.lang.reflect.Method getService()
-{
-return this.service;
-}
-public boolean isGetAllowed()
-{
-    return this.isGetAllowed;
-}
-public boolean isPostAllowed()
-{
-    return this.isPostAllowed;
-}
-public String getForwardTo()
-{
-    return this.forwardTo;
-}
-public boolean getRunOnStart()
-{
-    return this.runOnStart;
-}
-public int getPriority()
-{
-    return this.priority;
-}
+
+public class Service {
+    private Class serviceClass;
+    private String path;
+    private Method service;
+    private boolean isGetAllowed;
+    private boolean isPostAllowed;
+    private String forwardTo;
+    private boolean runOnStart;
+    private int priority;
+
+    public Service() {
+        this.serviceClass = null;
+        this.path = "";
+        this.service = null;
+        this.forwardTo = null;
+        this.isGetAllowed = false;
+        this.isPostAllowed = false;
+        this.runOnStart = false;
+        this.priority = 0;
+    }
+
+    public void setServiceClass(java.lang.Class serviceClass) {
+        this.serviceClass = serviceClass;
+    }
+
+    public java.lang.Class getServiceClass() {
+        return this.serviceClass;
+    }
+
+    public void setPath(java.lang.String path) {
+        this.path = path;
+    }
+
+    public void setIsGetAllowed(boolean isGetAllowed) {
+        this.isGetAllowed = isGetAllowed;
+    }
+
+    public void setIsPostAllowed(boolean isPostAllowed) {
+        this.isPostAllowed = isPostAllowed;
+    }
+
+    public void setForwardTo(String forwardTo) {
+        this.forwardTo = forwardTo;
+    }
+
+    public void setRunOnStart(boolean runOnStart) {
+        this.runOnStart = runOnStart;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public java.lang.String getPath() {
+        return this.path;
+    }
+
+    public void setService(java.lang.reflect.Method service) {
+        this.service = service;
+    }
+
+    public java.lang.reflect.Method getService() {
+        return this.service;
+    }
+
+    public boolean isGetAllowed() {
+        return this.isGetAllowed;
+    }
+
+    public boolean isPostAllowed() {
+        return this.isPostAllowed;
+    }
+
+    public String getForwardTo() {
+        return this.forwardTo;
+    }
+
+    public boolean getRunOnStart() {
+        return this.runOnStart;
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
 }
