e1f316628903f0137118cf93a0464a63d3cb9a8e
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-26 12:50:20 +0530

    Feat Parameterized constructor

--- WEB-INF/classes/com/thinking/machines/webrock/scopes/SessionScope.java
+++ WEB-INF/classes/com/thinking/machines/webrock/scopes/SessionScope.java
@@ -3,6 +3,11 @@ package com.thinking.machines.webrock.scopes;
 import javax.servlet.http.HttpSession;
 public class SessionScope {
     private HttpSession session;
+
+    public SessionScope(HttpSession session)
+    {
+        this.session=session;
+    }
     public void setAttribute(String name,Object object)
     {
         session.setAttribute(name,object);
