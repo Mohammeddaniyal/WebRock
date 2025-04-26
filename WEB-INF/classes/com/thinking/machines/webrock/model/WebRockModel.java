846610dcab044a95dee58bbb7011a12cf82e0f6a
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-26 12:00:36 +0530

    Increased printlns for verifying

--- WEB-INF/classes/com/thinking/machines/webrock/model/WebRockModel.java
+++ WEB-INF/classes/com/thinking/machines/webrock/model/WebRockModel.java
@@ -1,38 +1,45 @@
 package com.thinking.machines.webrock.model;
+
 import com.thinking.machines.webrock.pojo.*;
 import java.util.*;
-public class WebRockModel
-{
-private Map<String,Service> services;
-public WebRockModel()
-{
-this.services=new HashMap<>();
-}
-public void putService(Service service)
-{
-this.services.put(service.getPath(),service);
-}
-public Service getService(String path)
-{
-return this.services.get(path);
-}
-public void print()
-{
- for (Map.Entry<String, Service> entry : services.entrySet()) {
-                String key = entry.getKey();
-                Service service = entry.getValue();
 
-                // Print the key (service name or identifier)
-                System.out.println("Service Key: " + key);
+public class WebRockModel {
+    private Map<String, Service> services;
 
-                // Print serviceClass, serviceMethod, and servicePath
-                System.out.println("  Service Class: " + service.getServiceClass().getName());
-                System.out.println("  Service Method: " + service.getService().getName());
-                System.out.println("  Service Path: " + service.getPath());
-                System.out.println("  Service Forward To : "+service.getForwardTo());
-                System.out.println("  Service isGetAllowed : "+service.isGetAllowed());
-                System.out.println("  Service isPostAllowed : "+service.isPostAllowed());
-                System.out.println("-----------------------------");
-            }
-}
+    public WebRockModel() {
+        this.services = new HashMap<>();
+    }
+
+    public void putService(Service service) {
+        this.services.put(service.getPath(), service);
+    }
+
+    public Service getService(String path) {
+        return this.services.get(path);
+    }
+
+    public void print() {
+        for (Map.Entry<String, Service> entry : services.entrySet()) {
+            String key = entry.getKey();
+            Service service = entry.getValue();
+
+            // Print the key (service name or identifier)
+            System.out.println("Service Key: " + key);
+
+            // Print serviceClass, serviceMethod, and servicePath
+            System.out.println("  Service Class: " + service.getServiceClass().getName());
+            System.out.println("  Service Method: " + service.getService().getName());
+            System.out.println("  Service Path: " + service.getPath());
+            System.out.println("  Service Forward To : " + service.getForwardTo());
+            System.out.println("  Service isGetAllowed : " + service.isGetAllowed());
+            System.out.println("  Service isPostAllowed : " + service.isPostAllowed());
+            System.out.println("Service runOnStart: " + service.getRunOnStart());
+            System.out.println("Service priority: " + service.getPriority());
+            System.out.println("Inject Session Scope: " + service.getInjectSessionScope());
+            System.out.println("Inject Application Scope: " + service.getInjectApplicationScope());
+            System.out.println("Inject Request Scope: " + service.getInjectRequestScope());
+            System.out.println("Inject Application Directory: " + service.getInjectApplicationDirectory());
+            System.out.println("-----------------------------");
+        }
+    }
 }
