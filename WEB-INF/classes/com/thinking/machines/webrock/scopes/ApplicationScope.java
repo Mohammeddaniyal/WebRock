eba7f5ead1976e88d77082bc4adf6c78ebfd1ebd
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-26 12:48:11 +0530

    Feat Parameterized constructor

--- WEB-INF/classes/com/thinking/machines/webrock/scopes/ApplicationScope.java
+++ WEB-INF/classes/com/thinking/machines/webrock/scopes/ApplicationScope.java
@@ -3,6 +3,10 @@ import javax.servlet.ServletContext;
 public class ApplicationScope {
     private ServletContext servletContext;
     
+    public ApplicationScope(ServletContext servletContext)
+    {
+        this.servletContext=servletContext; 
+    }
     public void setAttribute(String name,Object object)
     {
         servletContext.setAttribute(name,object);
