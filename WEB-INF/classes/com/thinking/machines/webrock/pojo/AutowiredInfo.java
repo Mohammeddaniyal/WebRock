1060c9d350f4c0cac79b2350d6c6845c0e0244a3
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-27 11:33:06 +0530

    Feat new proeprty beanName

--- WEB-INF/classes/com/thinking/machines/webrock/pojo/AutowiredInfo.java
+++ WEB-INF/classes/com/thinking/machines/webrock/pojo/AutowiredInfo.java
@@ -2,12 +2,18 @@ package com.thinking.machines.webrock.pojo;
 
 import java.lang.reflect.*;
 public class AutowiredInfo {
+    private String beanName;
     private Field autowiredField;
     private Method setterMethod;
-    public AutowiredInfo(Field autowiredField,Method setterMethod)
+    public AutowiredInfo(String beanName,Field autowiredField,Method setterMethod)
     {
+        this.beanName=beanName;
         this.autowiredField=autowiredField;
         this.setterMethod=setterMethod;
+    }
+    public void setBeanName(String beanName)
+    {
+        this.beanName=beanName;
     }
     public void setAutowiredField(Field autowirField)
     {
@@ -16,6 +22,10 @@ public class AutowiredInfo {
     public void setSetterMethod(Method setterMethod)
     {
         this.setterMethod=setterMethod;
+    }
+    public String getBeanName()
+    {
+        return this.beanName;
     }
     public Field getAutowiredField()
     {
