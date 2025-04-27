63f5f3297bd616d572dd616330b93d8c484ea3e0
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-27 21:53:18 +0530

    Feat added this file to test autowired

---
+++ WEB-INF/classes/booby/test/AutowiredTest.java
@@ -0,0 +1,28 @@
+package booby.test;
+
+import com.thinking.machines.webrock.annotations.InjectSessionScope;
+import com.thinking.machines.webrock.annotations.Path;
+import com.thinking.machines.webrock.annotations.Forward;
+import com.thinking.machines.webrock.scopes.SessionScope;
+
+@InjectSessionScope
+@Path("/AutowiredTest")
+public class AutowiredTest {
+    private SessionScope session;
+    public void setSessionScope(SessionScope session)
+    {
+        System.out.println("Setting session scope");
+        this.session=session;
+    }
+    @Path("/sessionTest")
+    @Forward("/calculator/greet")
+    public void setStudentInSession()
+    {
+        System.out.println("HELLOOOOOOOOOOOO!!!");
+        Student s=new Student(100,"Daniyal Ali");
+        System.out.println("hi");
+        System.out.println(session);
+        session.setAttribute("std", s);
+        System.out.println("LOOW");
+    }
+}
