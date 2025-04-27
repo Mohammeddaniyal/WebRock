4203ac26e55d5c48fa866fe12a191a900d960476
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-04-27 10:36:42 +0530

    Feat added new pojo

---
+++ WEB-INF/classes/com/thinking/machines/webrock/pojo/Autiwired.java
@@ -0,0 +1,29 @@
+package com.thinking.machines.webrock.pojo;
+
+import java.lang.reflect.*;
+public class Autiwired {
+    private Field autowiredField;
+    private Method setterMethod;
+    public Autowired(Field autowiredField,Method setterMethod)
+    {
+        this.autowiredField=autowiredField;
+        this.setterMethod=setterMethod;
+    }
+    public void setAutowiredField(Field autowirField)
+    {
+        this.autowiredField=autowirField;
+    }
+    public void setSetterMethod(Method setterMethod)
+    {
+        this.setterMethod=setterMethod;
+    }
+    public Field getAutowiredField()
+    {
+        return this.autowiredField;
+    }
+    public Method getSetterMethod()
+    {
+        return this.setterMethod;
+    }
+    
+}
