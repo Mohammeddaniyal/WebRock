e3936370502bdef4fbf4857ad94678ef345efe0f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-05-01 13:39:16 +0530

    Import the package for @Rentention

--- WEB-INF/classes/com/thinking/machines/webrock/annotations/SecuredAccess.java
+++ WEB-INF/classes/com/thinking/machines/webrock/annotations/SecuredAccess.java
@@ -2,6 +2,7 @@ package com.thinking.machines.webrock.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
